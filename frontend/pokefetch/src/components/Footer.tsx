97250384b44933d4ef1d8c0d4308dc4e8f826aed
AnandhaSivam-00 <anandsugan218@gmail.com>
2025-11-07 16:59:32 +0530

    feat(ui): add Footer and CreditsModal components
    - Create CreditsModal component with info about the app and credits
    - Implement Footer component with copyright info, link to GitHub, and button to open CreditsModal
    - Replace inline footer JSX in index page with imported Footer component
    - Add lazy loading for Footer component in the index page
    - Include Pokéfetch logo in the Footer with styled positioning and opacity
    - Manage modal visibility state in Footer using useState hook

---
+++ frontend/pokefetch/src/components/Footer.tsx
@@ -0,0 +1,44 @@
+import { useState, type JSX } from 'react'
+
+import CreditsModal from './CreditsModal'
+
+import PokeFetchLogoOriginal from '../assets/pokefetch-icon-original.png';
+
+const Footer = (): JSX.Element => {
+    const [openCreditsModal, setOpenCreditsModal] = useState<boolean>(false);
+
+    return (
+        <>
+            <footer className='bg-black-background w-full h-auto px-3 py-2 pt-4 overflow-hidden'>
+                <div className='w-full relative bg-transparant z-1'>
+                    <h5 className='font-semibold!'>BY LITTLE THINKER</h5>
+
+                    <div className='w-full flex flex-col sm:flex-row justify-start sm:justify-between items-start gap-2 [&>span,a]:text-xs [&>span,a]:text-gray-4'>
+                        <span>@{new Date().getFullYear()}</span>
+                        <a href="http://github.com/AnandhaSivam-00/" target='_blank' className='underline hover:text-black-accent-2'>github.com/AnandhaSivam-00</a>
+                    </div>
+
+                    <button 
+                        className='text-[0.5rem] text-black-accent-1 underline hover:text-black-accent-2 mt-1 cursor-pointer'
+                        onClick={() => setOpenCreditsModal(true)}
+                    >
+                        About & Credits
+                    </button>
+
+                    <img
+                        src={PokeFetchLogoOriginal}
+                        alt='footer back logo'
+                        className='w-auto h-full object-cover absolute right-0 bottom-0 z-0 opacity-20 transform translate-x-5 translate-y-5 -rotate-30 pointer-events-none'
+                    />
+                </div>
+            </footer>
+
+            <CreditsModal 
+                isVisible={openCreditsModal} 
+                setOpenCreditsModal={setOpenCreditsModal}
+            />
+        </>
+    )
+}
+
+export default Footer
