acb84614d09ff0fde5cf1823e612b840dd58210e
AnandhaSivam-00 <anandsugan218@gmail.com>
2025-11-07 17:28:43 +0530

    docs(pokefetch): update meta tags and improve credits modal styling
    - Enhance meta description for SEO and user clarity
    - Update canonical link to point to the correct PokéFetch URL
    - Replace banner image formats from PNG to optimized WebP
    - Modify credits modal to add border and padding for better layout
    - Add text justification to the about section for improved readability

--- frontend/pokefetch/src/components/CreditsModal.tsx
+++ frontend/pokefetch/src/components/CreditsModal.tsx
@@ -8,16 +8,16 @@ type CreditsModalProps = {
 const CreditsModal = ({ isVisible, setOpenCreditsModal }: CreditsModalProps): JSX.Element => {
   return (
     <section className={`absolute inset-0 w-full min-h-screen bg-transparent p-3 flex flex-col justify-center items-center backdrop-blur-xl ${isVisible ? 'block' : 'hidden'}`}>
-        <div className='bg-black w-full sm:max-w-[30rem] h-auto p-3 flex flex-col justify-center items-center rounded-lg drop-shadow-2xl drop-shadow-gray-3'>
+        <div className='bg-black w-full sm:max-w-[30rem] h-auto p-3 flex flex-col justify-center items-center rounded-lg drop-shadow-2xl drop-shadow-gray-3 border-2 border-gray-2'>
             <button 
                 className='self-end px-2 pb-2 cursor-pointer hover:text-primary-accent-1'
                 onClick={() => setOpenCreditsModal(false)}
             >
                 X
             </button>
-            <div className='w-full flex flex-col justify-center items-start gap-y-3 [&>h3]:mb-0!'>
+            <div className='w-full flex flex-col justify-center items-start gap-y-3 [&>h3]:mb-0! px-2 md:px-5'>
                 <h3>About</h3>
-                <p>Pokéfetch is a chill React + TypeScript app that taps into the PokéAPI to pull in Pokémon info on the fly. Just search for a Pokémon, and boom — you get a clean, minimal card showing the essentials. No clutter, no chaos, just straight vibes and Pokédex energy.</p>
+                <p className='text-justify'>Pokéfetch is a chill React + TypeScript app that taps into the PokéAPI to pull in Pokémon info on the fly. Just search for a Pokémon, and boom — you get a clean, minimal card showing the essentials. No clutter, no chaos, just straight vibes and Pokédex energy.</p>
 
                 <h3 className='mt-4!'>Credits</h3>
                 <div className='w-full grid grid-cols-2 gap-x-3 pb-2 px-1'>
